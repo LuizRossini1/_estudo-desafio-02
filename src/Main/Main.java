174ae2f1dbb505d11d8ab2e6a73919e91a2d69c2
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-21 14:39:38 -0300

    instancing object and menu

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,6 +6,11 @@ public class Main {
     private int totalPurchases;
     private double cardLimit;
 
+    //setter
+    public void setCardLimit(double cardLimit) {
+        this.cardLimit = cardLimit;
+    }
+
     //getters
     public int getTotalPurchases() {
         return totalPurchases;
@@ -22,6 +27,11 @@ public class Main {
         //objects
         var myAccount = new Main();
 
-        System.out.println(myAccount.getCardLimit());
+        String menu =
+                "Welcomete to Alura shop! \n" +
+                        "Type your card limit: ";
+
+        System.out.println(menu);
     }
+
 }
