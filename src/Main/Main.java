a2f970db7ac5895daa37a148d8b4dcf5b5768782
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-21 14:31:21 -0300

    creating class skelet

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,27 @@
 package Main;
+import java.util.Scanner;
 
 public class Main {
+    //variables
+    private int totalPurchases;
+    private double cardLimit;
 
+    //getters
+    public int getTotalPurchases() {
+        return totalPurchases;
+    }
+
+    public double getCardLimit() {
+        return cardLimit;
+    }
+
+    public static void main(String[] args) {
+        //defining scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //objects
+        var myAccount = new Main();
+
+        System.out.println(myAccount.getCardLimit());
+    }
 }
